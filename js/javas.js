f89e09ef632b913b30afd776c3b4ad370e55b64d
PriscilaButzke <priscilabutzke@outlook.com>
2022-04-11 22:03:42 -0300

    Versão 1

---
+++ js/javas.js
@@ -0,0 +1,41 @@
+
+        const i = document.getElementById("img")
+
+        function power(){
+            i.src = "img/acesa.png"
+            i.style.width= "300px"
+        }
+
+        function imgamarela() {
+            i.src = "img/amarela.webp"
+            i.style.width= "200px"
+        }
+
+        function imgvermelha() {
+            i.src = "img/vermelha.png"
+            i.style.height= "auto"
+            i.style.width= "250px"
+        }
+
+        function imgazul() {
+            i.src ="img/azul.webp"
+        }
+
+        function imgverde() {
+            i.src ="img/verde.png"
+            i.style.width= "250px"
+            
+        }
+
+        function imglaranja() {
+            i.src = "img/laranja.webp"
+            i.style.width= "200px"
+            
+        }
+
+        function imgbranca() {
+            i.src = "img/branca.png"
+            i.style.width= "400px"
+        }
+
+        
